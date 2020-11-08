07516734eb0bceb9b5e2251e3161a22c6b1400d6
Hoil-Gang <khl1154@naver.com>
2020-11-09 01:56:56 +0900

    test

--- src/test/java/com/clone/chat/ChatApplicationTests.java
+++ src/test/java/com/clone/chat/ChatApplicationTests.java
@@ -3,11 +3,11 @@ package com.clone.chat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ChatApplicationTests {
 
-	@Test
+/*	@Test
 	void contextLoads() {
-	}
+	}*/
 
 }

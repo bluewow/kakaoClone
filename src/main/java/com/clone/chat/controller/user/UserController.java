48eecdedf2d29ae796d2ef1a49714b5ec71c1212
khl1154 <khl1154@naver.com>
2020-12-07 20:57:50 +0900

    세션 테스트

--- src/main/java/com/clone/chat/controller/user/UserController.java
+++ src/main/java/com/clone/chat/controller/user/UserController.java
@@ -64,6 +64,8 @@ public class UserController {
 
 		if("user1@daum.net".equals(dto.getId())&&"1234".equals(dto.getPw())){
 			resultMap.put("return","success");
+			session.getAttribute(dto.getNickName());
+			session.getAttribute(dto.getNickName());
 
 		}else{
 			resultMap.put("return","fail");
